8808c69fdfb01a076e3a0c6a04aa141417c13cea
navindu-sachintha <navindu.social@gmail.com>
2024-09-11 17:35:19 +0530

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,19 +10,18 @@ import NextAuth from "next-auth"
 const { auth } = NextAuth(authConfig)
 export default auth(async function middleware(req):Promise<any> {
     // Your custom middleware logic goes here
+    const { pathname } = req.nextUrl
     const isLoggedIn = !!req.auth
     console.log('ROUTE: ', req.nextUrl.pathname);
     console.log("Is Logged in: ", isLoggedIn);
 
     const isApiAuthRoute = req.nextUrl.pathname.startsWith(apiAuthPrefix);
     const isPublicRoutes = publicRoutes.includes(req.nextUrl.pathname);
-    const isAuthRoute = authRoutes.includes(req.nextUrl.pathname);
+    const isAdminRoute = pathname.startsWith('/admin')
 
-    if(isApiAuthRoute){
-        return null
-    }
 
-    if(isAuthRoute){
+
+    if(isAdminRoute){
         if(isLoggedIn){
             return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT,req.nextUrl))
         }
