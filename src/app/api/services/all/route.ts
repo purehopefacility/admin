8888ea4495470660e5fe6a49d5b14ac422b1945e
nawodaya piyumina <114112572+antiloger@users.noreply.github.com>
2024-09-13 14:13:29 +0530

    Update route.ts

--- src/app/api/services/all/route.ts
+++ src/app/api/services/all/route.ts
@@ -20,7 +20,7 @@ const getActiveServices = async () => {
       ServiceCategoryTable,
       eq(ServiceTable.ServiceCategory, ServiceCategoryTable.categoryId),
     )
-    // .where(eq(ServiceTable.status, "active"))
+    .where(eq(ServiceTable.status, "active"))
     .orderBy(desc(ServiceTable.serviceOrder));
 };
 
