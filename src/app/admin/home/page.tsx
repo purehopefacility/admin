02e5a63d76dd773e8d8fb9bad430185144675fd0
Nomin Sendinu <soloriderns@gmail.com>
2024-10-01 13:42:52 +0530

    fix:service active tab

--- src/app/admin/home/page.tsx
+++ src/app/admin/home/page.tsx
@@ -330,7 +330,7 @@ export default function Dashboard() {
           </DropdownMenu>
         </header>
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-          <Tabs defaultValue="all">
+          <Tabs defaultValue="services">
             <div className="flex items-center">
               <TabsList>
                 <TabsTrigger value="services">Services</TabsTrigger>
