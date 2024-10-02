092ecb4eb70a2dd506be4142d4aa928d97d5d47a
Nomin Sendinu <soloriderns@gmail.com>
2024-10-02 12:23:31 +0530

    merge:sql files merged

--- drizzle/0002_young_steel_serpent.sql
+++ drizzle/0002_young_steel_serpent.sql
@@ -23,3 +23,8 @@ CREATE TABLE IF NOT EXISTS "slide_images" (
 	"button_txt" varchar,
 	"button_link" varchar
 );
+
+ALTER TABLE "slide_images" RENAME COLUMN "image_order" TO "slide_order";--> statement-breakpoint
+ALTER TABLE "slide_images" RENAME COLUMN "image_desc_1" TO "slide_title_1";--> statement-breakpoint
+ALTER TABLE "slide_images" RENAME COLUMN "image_desc_2" TO "slide_title_2";--> statement-breakpoint
+ALTER TABLE "slide_images" ADD COLUMN "slide_desc" varchar;
