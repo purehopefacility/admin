55f556f3ea171355beb03b99570d98e675a6ba18
Nomin Sendinu <soloriderns@gmail.com>
2024-10-04 22:48:39 +0530

    fix:redeploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,7 @@ import NextAuth from "next-auth";
 // Use only one of the two middleware options below
 // 1. Use middleware directly
 // export const { auth: middleware } = NextAuth(authConfig)
+// fix:comment
 
 // 2. Wrapped middleware option
 const { auth } = NextAuth(authConfig);
