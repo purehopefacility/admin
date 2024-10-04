f8ce7031d568c7d011513a9389c35c382db12e65
Nomin Sendinu <soloriderns@gmail.com>
2024-10-04 15:24:26 +0530

    add:tracked getcustomers

---
+++ src/app/api/getcustomers/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { db } from "@/db/DB";
+
+import { CustomerTable } from "@/db/Schema";
+
+//this is usefull to 2 places
+//1) navbar icon set
+//2) ADMIN --> add feedback
+export async function GET() {
+  try {
+    const cdata = await db
+      .select({
+        name: CustomerTable.customerName,
+        customerID: CustomerTable.customerId,
+        phoneNo: CustomerTable.phoneNumber,
+      })
+      .from(CustomerTable);
+
+    return NextResponse.json(
+      { CustomerData: cdata ? cdata : [] },
+      { status: 200 },
+    );
+  } catch (e) {
+    console.log("error in gettinh customers" + e);
+    return NextResponse.json({ message: e }, { status: 500 });
+  }
+}
+export const dynamic = "force-dynamic";
